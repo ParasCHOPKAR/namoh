cd5ec043d5285da609892e63ca7e164af56c97dc
Paras Chopkar <paraschopkar0@gmail.com>
2026-05-11 12:03:58 +0530

    Fix: Added default export to storefront page.tsx

---
+++ app/(storefront)/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+// Import your components here once they are ready!
+// import HeroSection from "@/components/HeroSection";
+// import TopBrands from "@/components/TopBrands";
+
+export default function StorefrontPage() {
+  return (
+    <main className="flex min-h-screen flex-col">
+      {/* <HeroSection />
+        <TopBrands /> 
+      */}
+      <h1 className="sr-only">Namoh Crockery Mart</h1>
+    </main>
+  );
+}
